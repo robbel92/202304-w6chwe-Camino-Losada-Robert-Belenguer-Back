8cd8cb2c76072477d47ffad8f527d177df1d1ed1
Camino Losada <camino.losada.alonso@gmail.com>
2023-05-13 16:27:48 +0200

    Connect to api

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,9 @@
 import "../loadEnvironment.js";
-
+import mongoose from "mongoose";
 import express from "express";
 import morgan from "morgan";
-
 import createDebug from "debug";
+import chalk from "chalk";
 
 export const debug = createDebug("robots-api:root");
 
@@ -12,6 +12,19 @@ export const app = express();
 app.disable("x-powered-by");
 
 app.use(morgan("combined"));
+
+const mongoDbConnection = process.env.MONGODB_CONNECTION;
+
+if (!mongoDbConnection) {
+  debug(chalk.red("Missing environment variables. Exiting..."));
+  process.exit(1);
+}
+
+try {
+  await mongoose.connect(mongoDbConnection);
+} catch (error: unknown) {
+  debug(`Error connecting database ${(error as Error).message}`);
+}
 
 app.use((_req, _res) => {
   debug("hola");
